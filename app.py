9f630e3da8b2e07b96010bad7662b1859a80d16b
Camila2015 <167145122+Camila2015@users.noreply.github.com>
2024-08-15 10:07:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st 
+from PIL import Image
+
 st.title("Mi primera página")
+
 st.header ("Hola, como va todo")
 st.write ("diferente tipo de letra")
+image = Image.open("Interfaces cosmo guapo.jpeg")
