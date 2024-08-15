b78890e4ae90851f84dd8674e5d4b6df68e021a2
Camila2015 <167145122+Camila2015@users.noreply.github.com>
2024-08-15 10:25:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,22 @@ col1, col2 = st.colums(2)
 
 with col1:
   st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+  resp = st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write("correcto!")
+
+with col2:
+  st.subheader("Esta es la segunda columna")
+  modo = st.radio("Que modalidad es la principal en tu interfaz", ("visual", "auditiva", "táctil"))
+  if modo == "visual":
+    st.write("La vista es fundamental para tu interfaz")
+  if modo == "auditiva":
+    st.write("La audición es fundamental para tu interfaz")
+  if modo == "táctil":
+     st.write("El tacto es fundamental para tu interfaz")
+    
+
+
+
 
