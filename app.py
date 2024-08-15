e849adaeb129ad7a48e0fe286821e2ec55a20b18
Camila2015 <167145122+Camila2015@users.noreply.github.com>
2024-08-15 10:10:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ st.title("Mi primera página")
 
 st.header ("Hola, como va todo")
 st.write ("diferente tipo de letra")
-image = Image.open("Interfaces Cosmo guapo.jpeg")
+image = Image.open("Cosmo guapo.jpeg")
 
 st.image(image, caption="La Leyenda")
+
 
